81fd70fc75f9cdcd0de75e0df313e75521535620
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2026-01-10 14:07:12 +0500

    add forms page and archive page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import {Routes} from '@angular/router';
 import {redirectGuard} from './data/guard/redirect-guard';
 import {Auth} from './children/auth/auth';
 import {departmentHeadGuard} from './data/guard/department-head-guard';
+import {Forms} from './children/head/forms/forms';
 import {Reports} from './children/head/reports/reports';
+import {Archive} from './children/head/archive/archive';
 import {Dashboard} from './children/head/dashboard/dashboard';
 
 export const routes: Routes = [
@@ -20,6 +22,21 @@ export const routes: Routes = [
         path: 'department-head',
         component: Dashboard,
         canActivate: [departmentHeadGuard],
+        children: [
+            {
+                path: 'forms',
+                component: Forms,
+            },
+            {
+                path: 'archive',
+                component: Archive,
+            },
+            {
+                path: '',
+                redirectTo: 'forms',
+                pathMatch: 'full',
+            }
+        ]
     },
     {
         path: 'department-head/reports',
---
+++ src/app/children/head/forms/forms.ts
@@ -0,0 +1,41 @@
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {NgIf} from '@angular/common';
+import {filter} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+
+@Component({
+    selector: 'app-forms',
+    imports: [
+        FormsModule,
+        RouterOutlet,
+        NgIf,
+    ],
+    templateUrl: './forms.html',
+    styleUrl: './forms.css',
+})
+export class Forms implements OnInit {
+    protected isFormTypeActive: boolean = false;
+    private readonly _router: Router = inject(Router);
+    private readonly _destroyRef: DestroyRef = inject(DestroyRef);
+
+    public ngOnInit(): void {
+        this.checkIfFormTypeActive(this._router.url);
+
+        this._router.events.pipe(
+            filter((event) => event instanceof NavigationEnd),
+            takeUntilDestroyed(this._destroyRef)
+        ).subscribe((event: NavigationEnd): void => {
+            this.checkIfFormTypeActive(event.url);
+        });
+    }
+
+    protected navigateToFormType(type: string): void {
+        this._router.navigate(['/department-head/forms', type]);
+    }
+
+    private checkIfFormTypeActive(url: string): void {
+        this.isFormTypeActive = /type-\d/.test(url);
+    }
+}
