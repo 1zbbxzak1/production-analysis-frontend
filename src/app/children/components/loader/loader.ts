f5a4c0c665532c00f1e4394bf1531525a4c809aa
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2026-01-11 14:41:43 +0500

    add loader

---
+++ src/app/children/components/loader/loader.ts
@@ -0,0 +1,22 @@
+import {Component, Input} from '@angular/core';
+import {NgIf} from '@angular/common';
+
+@Component({
+    selector: 'app-loader',
+    imports: [
+        NgIf
+    ],
+    templateUrl: './loader.html',
+    styleUrl: './loader.css',
+})
+export class Loader {
+    @Input()
+    public isLoading: boolean | (() => boolean) = false;
+
+    public get loading(): boolean {
+        if (typeof this.isLoading === 'function') {
+            return this.isLoading();
+        }
+        return this.isLoading as boolean;
+    }
+}
